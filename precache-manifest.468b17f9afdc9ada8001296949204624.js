52ed9de92469200f10fcb4cdfb475df71ce1f9af
unknown <malhotra.jatin1.jm@gmail.com>
2020-08-24 00:59:47 +0530

    Updates

---
+++ precache-manifest.468b17f9afdc9ada8001296949204624.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1840078f857cb9cf802e1bd36ad99da5",
+    "url": "/Flipkart-clone/index.html"
+  },
+  {
+    "revision": "782eb3f0a4edb0be5670",
+    "url": "/Flipkart-clone/static/css/2.3430a301.chunk.css"
+  },
+  {
+    "revision": "6564e5461490dd52e73c",
+    "url": "/Flipkart-clone/static/css/main.42b173ca.chunk.css"
+  },
+  {
+    "revision": "782eb3f0a4edb0be5670",
+    "url": "/Flipkart-clone/static/js/2.3698d5c4.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/Flipkart-clone/static/js/2.3698d5c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6564e5461490dd52e73c",
+    "url": "/Flipkart-clone/static/js/main.06193c01.chunk.js"
+  },
+  {
+    "revision": "3065145a0d2e72b5e967",
+    "url": "/Flipkart-clone/static/js/runtime-main.7b24723b.js"
+  },
+  {
+    "revision": "72b8b983ac6c00dfdaa6159743f25520",
+    "url": "/Flipkart-clone/static/media/ad1.72b8b983.jpg"
+  },
+  {
+    "revision": "b3f49afdaa769bc2cb267d590e308ae0",
+    "url": "/Flipkart-clone/static/media/bars-solid.b3f49afd.svg"
+  },
+  {
+    "revision": "3718be2e64e3cc3991ac73a74a5e72f2",
+    "url": "/Flipkart-clone/static/media/jbl.3718be2e.jpg"
+  },
+  {
+    "revision": "8bf1af37dd02198b256725e83a203795",
+    "url": "/Flipkart-clone/static/media/shopping-cart-solid.8bf1af37.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/Flipkart-clone/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/Flipkart-clone/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/Flipkart-clone/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/Flipkart-clone/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "2c5552a10c3ab0a115c9484f908f982d",
+    "url": "/Flipkart-clone/static/media/times-solid.2c5552a1.svg"
+  }
+]);
